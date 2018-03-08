1a8e9391345346bc40ea7c7ad55711a7a9e883b6
Akash Bhandwalkar <akash.bhandwalkar@cronj.com>
2018-03-09 00:42:36 +0530

    mongodb connection

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,57 +1,16 @@
 // const MongoClient = require('mongodb').MongoClient;
 
-const { MongoClient, ObjectID } = require('mongodb');
-var obj = new ObjectID();
-console.log("obj-->", obj);
+const { MongoClient } = require('mongodb');
 
 MongoClient.connect("mongodb://localhost:27017/TodoApp", (error,client)=>{
     
 if(error){
         return console.log("Unable to conncect", error);
     }else{
-        // console.log("conncected-->",client);
+        console.log("conncected-->",client);
 
     };
-    const db = client.db("TodoApp");
-    // db.collection('Todos').insertOne({
-    //     id:1,
-    //     name: "Akash Bhandwalkar"
-    // }, (error, result)=>{
-    //     if(error){
-    //         console.log("unable to save");
-    //     }else{
-    //         console.log("successfully executed the function!", JSON.stringify(result));
-    //     }
-    // });
-
-    // db.collection("Todos").find({
-    //     name:"Akash Bhandwalkar"
-    // }).toArray().then((docs)=>{
-    //     console.log("docs-->", JSON.stringify(docs, undefined, 4));
-    // },(error)=>{
-    //     console.log("error-->", error);
-    // })
-
-    // db.collection('Todos').deleteMany({
-    //     name:"Akash Bhandwalkar"
-    // }).then((result)=>{
-    //     console.log(JSON.stringify(result, undefined,2));
-    // })
-
-    //  db.collection('Todos').deleteOne({
-    //     name:"Akash Bhandwalkar"
-    // }).then((result)=>{
-    //     console.log(JSON.stringify(result, undefined,2));
-    // });
-
-    db.collection('Todos').findOneAndDelete({
-        flag:true
-    }).then( (result)=>{
-        console.log(JSON.stringify(result, undefined, 2));
-    })
-    .catch( (error)=>{
-        console.log("error-->", error);
-    })
+    
 
     client.close();
 });
