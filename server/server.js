13b5049a2102d8f5ef5bae458d666804e501f450
Akash Bhandwalkar <akash.bhandwalkar@cronj.com>
2018-03-09 13:26:22 +0530

    get todo by id api is done

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/Todo');
 var { User } = require('./models/User')
-
+var  { ObjectID } = require('mongodb');
 var app = express();
 app.use(bodyParser()); //middleware convert string req.body to json
 
@@ -32,6 +32,24 @@ app.get('/todos', (req, res)=>{
     },(e)=>{
         res.status(400).send(e);
     });
+})
+
+//get todos by id
+app.get('/todos/:id', (req, res)=>{
+    if(ObjectID.isValid(req.params.id)){
+
+        Todo.findById(req.params.id).then( (docs)=>{
+            if(docs){
+                res.status(200).send(docs)
+            }else{
+                res.status(400).send("No Todo");
+            }
+        }).catch((e)=>{
+            res.status(400).send(e);
+        })
+    }else{
+        res.status(500).send("Not valid object id");
+    }
 })
 
 app.listen(3000, ()=>{
